20d02b782bc6c1cddd25ca0121743fe0c19f598c
Jashan042 <162995854+Jashan042@users.noreply.github.com>
2024-03-11 19:20:14 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import axios from 'axios';
-import Weather from './Weather';
+
 
 function App() {
   const [city, setCity] = useState('');
